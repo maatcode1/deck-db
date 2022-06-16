bba25b44b82394cd3a5f6a1561f89f33834bc0db
Casper <maatcode@gmail.com>
2022-06-16 16:16:00 +0200

    Removed Laminas adapter

--- src/Adapter.php
+++ src/Adapter.php
@@ -1,18 +1,13 @@
 <?php
 
-namespace Deck\Database;
-
-use Laminas\Db\Adapter\AdapterInterface;
-use Laminas\Db\Adapter\Driver;
-use Laminas\Db\Adapter\Platform;
+namespace Deck\Db;
 
 class Adapter
 {
-    protected AdapterInterface $adapter;
+    protected $adapter;
 
-    public function setAdapter (AdapterInterface $adapter) {
+    public function setAdapter ($adapter) {
         $this->adapter = $adapter;
     }
-
 
 }
